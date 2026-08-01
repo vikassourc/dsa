6cc0bbf08a4575b9d123fad3eb2f867f4b156c72
vikassourc <vsrivastava873@gmail.com>
2026-08-01 19:22:16 +0530

    Time: 2 ms (47.09%) | Memory: 12.8 MB (40.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,35 +1,34 @@
-#include <vector>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
-    stack<int> st;
-    vector<int> nge;
-
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n2 = nums2.size();
-        vector<int> nge2(n2, -1);  // with intial value -1
 
-        // Step 1: Calculate NGE for nums2 using the monotonic stack approach
-        for (int i = n2 - 1; i >= 0; --i) {
-            while (!st.empty() && st.top() <= nums2[i])
+        stack<int> st;
+        unordered_map<int,int> mp;
+
+        // Find next greater for every element in nums2
+        for(int i = nums2.size()-1; i >= 0; i--)
+        {
+            while(!st.empty() && st.top() <= nums2[i])
+            {
                 st.pop();
-            if (!st.empty())
-                nge2[i] = st.top();
+            }
+
+            if(st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+
             st.push(nums2[i]);
         }
 
-        // Step 2: For every nums1[i], find its index in nums2 and take result from nge2
-        nge.resize(nums1.size(), -1);
-        for (int i = 0; i < nums1.size(); ++i) {
-            for (int j = 0; j < n2; ++j) {
-                if (nums1[i] == nums2[j]) {
-                    nge[i] = nge2[j];
-                    break;
-                }
-            }
+        vector<int> ans;
+
+        // Get answers for nums1
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            ans.push_back(mp[nums1[i]]);
         }
-        return nge;
+
+        return ans;
     }
 };
