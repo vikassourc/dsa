85bd4f428722907f71ef37c9441b30c134c1c1a4
vikassourc <vsrivastava873@gmail.com>
2026-08-02 09:40:14 +0530

    Time: 7 ms (35.23%) | Memory: 19.4 MB (31.69%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
+    bool stoneGame(vector<int>& piles) {
 
-    int fun(vector<int>& piles, int i, int j,
-            vector<vector<int>>& dp)
-    {
-        if(i == j)
-        {
-            return piles[i];
-        }
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        int left = piles[i] - fun(piles, i + 1, j, dp);
-
-        int right = piles[j] - fun(piles, i, j - 1, dp);
-
-        return dp[i][j] = max(left, right);
-    }
-
-    bool stoneGame(vector<int>& piles)
-    {
         int n = piles.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        return fun(piles, 0, n - 1, dp) > 0;
+        // Base case
+        for(int i = 0; i < n; i++)
+        {
+            dp[i][i] = piles[i];
+        }
+
+        // Fill table
+        for(int len = 2; len <= n; len++)
+        {
+            for(int i = 0; i + len - 1 < n; i++)
+            {
+                int j = i + len - 1;
+
+                int left = piles[i] - dp[i + 1][j];
+
+                int right = piles[j] - dp[i][j - 1];
+
+                dp[i][j] = max(left, right);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
     }
 };
