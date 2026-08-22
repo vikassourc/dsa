0e67f94ad35fd30fcae9cc1baa8e8de3e93a47b0
vikassourc <vsrivastava873@gmail.com>
2026-08-22 21:45:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int perfectSum(vector<int>& arr, int sum) {
+
+        int n = arr.size();
+
+        vector<vector<int>> dp(n+1);
+
+        for(int i=0; i<=n; i++)
+        {
+            vector<int> t(sum+1, 0);
+            dp[i] = t;
+        }
+
+        // With no elements, only sum 0 is possible
+        dp[n][0] = 1;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=0; j<=sum; j++)
+            {
+                if(arr[i] > j)
+                {
+                    dp[i][j] = dp[i+1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i+1][j-arr[i]] + dp[i+1][j];
+                }
+            }
+        }
+
+        return dp[0][sum];
+    }
+};
