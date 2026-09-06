8a5cce3cb0f7db1046a194362bec035254bd8190
vikassourc <vsrivastava873@gmail.com>
2026-09-07 01:06:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,46 +1,43 @@
 class Solution {
-public:
-    vector<int> dijkstra(int n, vector<vector<int>> &edges, int src) {
-        vector<vector<pair<int, int>>> adj(n);
-        vector<int> dist(n, INT_MAX);
-
-        for (int i = 0; i < edges.size(); i++) {
-            int s = edges[i][0];
-            int d = edges[i][1];
-            int w = edges[i][2];
-
-            adj[s].push_back({d, w});
-            adj[d].push_back({s, w});
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int,int>>>a(V);
+        for(int i=0;i<edges.size();i++)
+        {
+            int s=edges[i][0];
+            int d=edges[i][1];
+            int w=edges[i][2];
+            a[s].push_back({d,w});
+            a[d].push_back({s,w});
+            
         }
-
-        priority_queue<pair<int, int>,
-                       vector<pair<int, int>>,
-                       greater<pair<int, int>>> pq;
-
-        dist[src] = 0;
-        pq.push({0, src});
-
-        while (!pq.empty()) {
-            pair<int, int> p = pq.top();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>dis(V,INT_MAX);
+        dis[src]=0;
+        pq.push({0,src});
+        while(!pq.empty())
+        {
+            pair<int,int>p=pq.top();
             pq.pop();
-
-            int d = p.first;
-            int node = p.second;
-
-            if (d > dist[node])
+            int d=p.first;
+            int node=p.second;
+            if(d>dis[node])
+            {
                 continue;
-
-            for (int j = 0; j < adj[node].size(); j++) {
-                int neigh = adj[node][j].first;
-                int weight = adj[node][j].second;
-
-                if (d + weight < dist[neigh]) {
-                    dist[neigh] = d + weight;
-                    pq.push({d + weight, neigh});
+            }
+            for(int j=0;j<a[node].size();j++)
+            {
+                int neigh=a[node][j].first;
+                int wt=a[node][j].second;
+                if(d+wt<dis[neigh])
+                {
+                    dis[neigh]=wt+d;
+                    pq.push({d+wt,neigh});
                 }
             }
         }
-
-        return dist;
+        return dis;
+        
     }
 };
